6dee3ab39b89beb9ca654de44ffe826f6896f73e
ShadowOfNight <36479507+ShadowOfNight@users.noreply.github.com>
2026-08-07 13:35:20 -0400

    Couple fixes
    - Removed bindingRedirect line in App.config as this was causing a conflict with MSBuild attempting to build for version 4.1.6.0.  This fixes the warning being spat out on compile
    - Fixed SharpDX and SharpDX.Mathematics in packages.config to use .net 4.8 framework like everything else instead of targeted 4.6.1 for some reason
    - Updated AssemblyInfo.cs to add Litestone Studios to company line, updated benji copyright to 2026, and bumped version number up to 0.8.0.0 due to the addition of GLTF support

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("LOMNTool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Litestone Studios")]
 [assembly: AssemblyProduct("LOMNTool")]
-[assembly: AssemblyCopyright("Copyright © benji 2018")]
+[assembly: AssemblyCopyright("Copyright © benji 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.1.0")]
-[assembly: AssemblyFileVersion("0.7.1.0")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
